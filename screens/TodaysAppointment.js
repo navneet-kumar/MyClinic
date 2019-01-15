b52f74a6d13a3a83d2667f4c8f95577a04f7c1e8
Navneet kumar <ts-navneet.kumar@rakuten.com>
2019-01-15 15:08:34 +0900

    Added screenshot & ShowAppointments component

--- screens/TodaysAppointment.js
+++ screens/TodaysAppointment.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Styles from "../components/Style";
 import Constants from "../components/Constants";
-import { Font, AppLoading } from "expo";
+import { FlatList } from "react-native";
 import {
   Container,
   Header,
@@ -10,15 +10,20 @@ import {
   Icon,
   Left,
   Right,
-  Body
+  Body,
+  List,
+  ListItem,
+  Text,
+  Item
 } from "native-base";
+import AppointmentCard from "../components/AppointmentCard";
 
 const appointments = [
   {
     name: "brayn",
     mobile: "+91 8090898872",
     gender: "male",
-    age: "43",
+    age: 43,
     description: "RCT week 3",
     appointment: ""
   },
@@ -26,7 +31,7 @@ const appointments = [
     name: "john",
     mobile: "+91 8090898872",
     gender: "male",
-    age: "23",
+    age: 23,
     description: "Clean up",
     appointment: ""
   },
@@ -34,7 +39,7 @@ const appointments = [
     name: "Maria",
     mobile: "+91 8090898872",
     gender: "female",
-    age: "18",
+    age: 18,
     description: "Checkup",
     appointment: ""
   },
@@ -42,7 +47,7 @@ const appointments = [
     name: "kaun",
     mobile: "+91 8090898872",
     gender: "male",
-    age: "53",
+    age: 53,
     description: "No info",
     appointment: ""
   },
@@ -50,7 +55,7 @@ const appointments = [
     name: "Julie",
     mobile: "+91 8090898872",
     gender: "female",
-    age: "35",
+    age: 35,
     description: " RCT week 3",
     appointment: ""
   }
@@ -72,51 +77,54 @@ export default class TodaysAppointment extends React.Component {
   }
 
   render() {
-    if (this.state.loading) {
-      return <AppLoading />;
-    } else {
+    return (
+      <Container>
+        <Header style={{ backgroundColor: Constants.theme_color }}>
+          <Left>
+            <Icon
+              type="FontAwesome"
+              name="bookmark"
+              style={[
+                Styles.iconStyle,
+                { color: Constants.theme_compliment_color }
+              ]}
+            />
+          </Left>
+          <Body style={{ flex: 3 }}>
+            <Title style={{ color: Constants.theme_compliment_color }}>
+              Today's Appointments
+            </Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <ShowAppointments appointments={appointments} />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+class ShowAppointments extends React.Component {
+  render() {
+    if (this.props.appointments.length > 0) {
       return (
-        <Container>
-          <Header style={{ backgroundColor: Constants.theme_color }}>
-            <Left>
-              <Icon
-                type="FontAwesome"
-                name="bookmark"
-                style={[
-                  Styles.iconStyle,
-                  { color: Constants.theme_compliment_color }
-                ]}
-              />
-            </Left>
-            <Body style={{ flex: 3 }}>
-              <Title style={{ color: Constants.theme_compliment_color }}>
-                Today's Appointments
-              </Title>
-            </Body>
-            <Right />
-          </Header>
-          <Content />
-        </Container>
-        // <SafeAreaView style={Styles.container}>
-        //   <ScrollView contentContainerStyle={Styles.form}>
-        //     <Text style={Styles.title}>Today's Appointments</Text>
-        //     <View style={Styles.card}>
-        //       {appointments.map((patient, index) => {
-        //         return (
-        //           <Card
-        //             key={index + 1}
-        //             name={patient.name}
-        //             age={Number(patient.age)}
-        //             mobile={patient.mobile}
-        //             gender={patient.gender}
-        //             description={patient.description}
-        //           />
-        //         );
-        //       })}
-        //     </View>
-        //   </ScrollView>
-        // </SafeAreaView>
+        <FlatList
+          data={this.props.appointments}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <AppointmentCard
+              name={item.name}
+              mobile={item.mobile}
+              age={item.age}
+              gender={item.gender}
+              description={item.description}
+            />
+          )}
+        />
       );
+    } else {
+      return <Title>No Appointments</Title>;
     }
   }
 }
